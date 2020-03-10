06321330ccaafbe0e62980dd279cc88cdc183a6c
vandekul <susannafm@gmail.org>
2020-03-10 14:09:49 +0100

    Initial

--- __openerp__.py
+++ __openerp__.py
@@ -1,24 +1,4 @@
 {
-    'name': "Bitcoin Payment Acquirer",
-    'version': '9.0',
-    'category' : 'Payment',
-    'website': 'https://github.com/mumaker',
-    'summary':  """Bitcoin Payment Gateway.""",
-    'description': """Payment Acquirer: Bitcoin""",
-    'author': 'mumaker',
-    'depends': ['payment', 'base', 'website_sale'],
-    'data': [
-        'views/views.xml',
-        'views/template.xml',
-        'data/bitcoin.xml',
-    ],
-    'images': ['static/description/icon.png'],
-    'license': 'LGPL-3',
-    'installable': True,
-    'application': True
-}
-
-{
     'name': "Gateway BitPay",
 
     'summary': """This module integrates BitPay - pay with Bitcoin, Litecoin, Ethereum, Ripple or similar Cryptocurrency - with Odoo v9.0""",
